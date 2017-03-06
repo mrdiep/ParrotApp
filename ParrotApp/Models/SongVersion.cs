be1dbd9e8d3495cd98dce1b8bf46df065a62dd90
DiepNguyenVan <mr.vdiep@gmail.com>
2017-03-06 22:25:59 +0700

    refactor code

--- ParrotApp/Models/SongVersion.cs
+++ ParrotApp/Models/SongVersion.cs
@@ -2,25 +2,23 @@
 {
     public class SongVersion
     {
-        //"id" INTEGER PRIMARY KEY,
-        //"songId" INTEGER,
-        //"chord" TEXT,
-        //"content" TEXT,
-        //"description" TEXT,
-        //"star" TEXT,
-        //"votes" TEXT,
-        //"updated" TEXT,
-        //"default" INTEGER
-
         public int Id { get; set; }
         public SongMetadata Metadata { get; set; }
         public string Chord { get; set; }
         public int VersionId { get; set; }
         public string Content { get; set; }
-        public string Description { get; set; }
+
+        private string _description;
+        public string Description { get { return _description; } set { _description = FilterDescription(value); } }
         public string Star { get; set; }
         public string Votes { get; set; }
         public string Updated { get; set; }
         public string Default { get; set; }
+
+        public static string FilterDescription(string text)
+        {
+            if (text == null) return string.Empty;
+            return text.Replace('\n', ' ');
+        }
     }
 }
--- ParrotApp/Views/HomePageSlideMenu.xaml.cs
+++ ParrotApp/Views/HomePageSlideMenu.xaml.cs
@@ -10,14 +10,10 @@ namespace ParrotApp.Views
         {
             InitializeComponent();
             this.IsFullScreen = true;
-            // You must set WidthRequest in this case
             this.WidthRequest = 350;
             this.MenuOrientations = MenuOrientation.LeftToRight;
             AnimationDurationMillisecond = 250;
             this.BackgroundColor = Color.White;
-            
-
-            // This is shadow view color, you can set a transparent color
             this.BackgroundViewColor = Color.FromRgba(0,0,0,111);
         }
     }
